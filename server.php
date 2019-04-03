3e0e584512ef8992b69262ebad3292f62c3430c2
Daniil Gentili <daniil.gentili.dg@gmail.com>
2019-04-03 16:01:22 +0000

    Listen on all interfaces

--- server.php
+++ server.php
@@ -6,6 +6,6 @@ use danog\Merger\MergerServer;
 use danog\Merger\Settings;
 
 $settings = new Settings();
-$settings->setTunnelEndpoint('127.0.0.1', 4444);
+$settings->setTunnelEndpoint('0.0.0.0', 4444);
 $server = new MergerServer($settings);
 $server->loop();
